04f131bec48e034b5d645db57175955f9f9d3cf0
Jelle Stuurman <jellejstuurman@gmail.com>
2020-02-10 20:12:47 +0100

    bilinear intersection for comparable results

--- intersect_raster.R
+++ intersect_raster.R
@@ -1,6 +1,6 @@
 intersect_raster <- function(ras, point){
   print("Intersecting raster. Getting elevation...")
-  my_elevation <- raster::extract(ras, point.sp)
+  my_elevation <- raster::extract(ras, point, method = "bilinear")
   return (my_elevation)
 }
  
