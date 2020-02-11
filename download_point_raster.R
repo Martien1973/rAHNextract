1f4fb1f519b09b45c158c29deea97aa01e6f0be6
Jelle Stuurman <jellejstuurman@gmail.com>
2020-02-11 13:40:59 +0100

    NA value fix

--- download_point_raster.R
+++ download_point_raster.R
@@ -3,7 +3,7 @@ download_point_raster <- function(wcsUrl, name = "elevation"){
         image <- download.file(wcsUrl, image_name)
         print("Download raster image succeeded.")
         my_raster <- raster(image_name)
-        NAvalue(my_raster) <- --32768.0
+        NAvalue(my_raster) <- -32768.0
         plot(my_raster, xlab="RD X", ylab="RD Y", main="Elevation (m)")
         ras <- list("raster" = my_raster, "file" = image_name)
         return(ras)
